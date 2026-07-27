c0de18ad24820586c675e4df6d68d6ca080e67f5
Adam <adomaislah@gmail.com>
2026-07-27 17:16:04 +0300

    Stage 2: read endpoints

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Temporary in-memory task list
+const tasks = [
+    {
+        id: 1,
+        title: "Morning shower",
+        done: true
+    },
+    {
+        id: 2,
+        title: "Workout",
+        done: false
+    },
+    {
+        id: 3,
+        title: "Have lunch",
+        done: false
+    }
+];
+
 // Information about the API
 app.get("/", (req, res) => {
     res.json({
@@ -17,6 +36,26 @@ app.get("/health", (req, res) => {
     res.json({
         status: "ok"
     });
+});
+
+// Return all tasks
+app.get("/tasks", (req, res) => {
+    res.json(tasks);
+});
+
+// Return one task using its ID
+app.get("/tasks/:id", (req, res) => {
+    const taskId = Number(req.params.id);
+
+    const task = tasks.find((task) => task.id === taskId);
+
+    if (!task) {
+        return res.status(404).json({
+            error: `Task ${taskId} not found`
+        });
+    }
+
+    res.json(task);
 });
 
 app.listen(port, () => {
